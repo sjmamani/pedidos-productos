34b949ab8b7f52786e65ed3229703da4d5c9755d
nicoomarquez <niko_98_99@hotmail.com>
2019-05-23 20:29:11 -0300

    Buscar pedidos por ID

--- src/components/Pedidos/Pedidos.js
+++ src/components/Pedidos/Pedidos.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react'
+import axios from "axios";
 
 export class Pedidos extends Component {
+
+    constructor(props){
+        super(props);
+        this.state =  {
+            pedido : null,
+            error : false,
+            cuit : ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        
+    }
+
+    
+    handleChange(event) {
+        this.setState({cuit : event.target.value});
+    }
     render() {
+
+        const buscarPedido = () => {
+
+          axios
+          .get("http://localhost:8080/pedido/"+ this.state.cuit)
+          .then(response => {
+            this.setState({ pedido: response.data });
+            console.log(response.data);
+          })
+          .catch(error => {
+            this.setState({ error: true });
+          });
+          console.log("buscando");
+        };
+
+        
         return (
-            <div>
-                
-            </div>
+            
+            <form>
+                <div className="form-group row">
+                    <div className="col-6">
+                        <label>Numero de pedido:</label>
+                        <input
+                        type="number"
+                        className="form-control"
+                        id="nroPedido"
+                        placeholder="Numero de pedido"
+                        min="0"
+                        value = {this.state.cuit}
+                        onChange={this.handleChange}
+                        />
+                        <button type="button" className="btn btn-primary" onClick={buscarPedido}>
+                         Buscar
+                        </button>
+                    </div>
+               </div>
+               <div>                    
+                    
+                </div>
+            </form>
         )
     }
 }

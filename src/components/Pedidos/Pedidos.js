73e9738fd7542f9c55a705ba1eaff29de3a448f0
Santiago, Mamani <santiagojmamani@gmail.com>
2019-05-23 22:02:13 -0300

    Tabla de items pedidos

--- src/components/Pedidos/Pedidos.js
+++ src/components/Pedidos/Pedidos.js
@@ -1,134 +1,134 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
 
 export class Pedidos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pedido: null,
+      error: false,
+      cuit: "",
+      loading: false
+    };
 
-    constructor(props){
-        super(props);
-        this.state =  {
-            pedido : null,
-            error : false,
-            cuit : '',
-            loading : false
-        };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-        this.handleChange = this.handleChange.bind(this);
-        
+  handleChange(event) {
+    this.setState({ cuit: event.target.value });
+  }
+  render() {
+    const buscarPedido = () => {
+      this.setState({ loading: true });
+      axios
+        .get("http://localhost:8080/pedido/" + this.state.cuit)
+        .then(response => {
+          this.setState({ pedido: response.data, loading: false });
+          console.log(response.data);
+        })
+        .catch(error => {
+          this.setState({ error: true, loading: false });
+        });
+    };
+
+    let pedido = null;
+    let items = null;
+
+    if (this.state.error) {
+      pedido = <p>No se encontro el pedido</p>;
+    } else {
+      if (this.state.loading) {
+        pedido = (
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        );
+      }
     }
 
-    /*buscarPedido(){
-        
-    }*/
-    
-    handleChange(event) {
-        this.setState({cuit : event.target.value});
-    }
-    render() {
-
-        const buscarPedido = () => {
-            this.setState({loading : true});
-          axios
-          .get("http://localhost:8080/pedido/"+ this.state.cuit)
-          .then(response => {
-            this.setState({ pedido: response.data,loading:false});
-            console.log(response.data);
-          })
-          .catch(error => {
-            this.setState({ error: true,loading:false });
-          });
-          console.log("buscando");
-        };
-
-        let pedido = null;
-        
-          if(this.state.error){
-            pedido = <p>No se encontro el pedido</p>
-          }
-          else{
-            if (this.state.loading){
-              pedido =(
-                <div className="spinner-border" role="status">
-                  <span className="sr-only">Loading...</span>
-                </div>)
-            }
-          }
-        
-
-          if (this.state.pedido) {
-            pedido = (
-                <table className = "table table-bordered">
-                    <tbody key={this.state.pedido.numeroPedido}>
-                    <tr>
-                        <td >Numero de pedido:</td>
-                        <td>{this.state.pedido.numeroPedido}</td>
-                    </tr>   
-                    <tr>
-                        <td >Cliente:</td>
-                        <td>{this.state.pedido.cliente.nombre}</td>
-                    </tr>
-                    <tr>
-                        <td>Estado:</td>
-                        <td>{this.state.pedido.estado}</td>
-                    </tr>
-                    <tr>
-                        <td>Fecha:</td>
-                        <td>{this.state.pedido.fechaPedido}</td>                                       
-                    </tr>
-                    </tbody>
-                </table>
-            );
-            const i = this.state.pedido.items.map(producto => {
-                return(
-                    <h1></h1>
-                );
-            })
-            let items = (
-                <div>
-                    <p>Tus pedidos:</p>
-                    <table>
-                        <thead> 
-                            <tr> 
-                                <th>Nombre producto</th>
-                                <th>Precio unitario</th>
-                                <th>Cantidad</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {i}
-                        </tbody>
-                    </table>
-                </div>
-
-              ); 
-
-          
+    if (this.state.pedido) {
+      pedido = (
+        <table className="table table-bordered">
+          <tbody key={this.state.pedido.numeroPedido}>
+            <tr>
+              <td>Numero de pedido</td>
+              <td>{this.state.pedido.numeroPedido}</td>
+            </tr>
+            <tr>
+              <td>Cliente</td>
+              <td>{this.state.pedido.cliente.nombre}</td>
+            </tr>
+            <tr>
+              <td>Estado</td>
+              <td>{this.state.pedido.estado}</td>
+            </tr>
+            <tr>
+              <td>Fecha</td>
+              <td>{this.state.pedido.fechaPedido}</td>
+            </tr>
+          </tbody>
+        </table>
+      );
+      const i = this.state.pedido.items.map((i, index) => {
         return (
-            
-            <form>
-                <div className="form-group row">
-                    <div className="col-6">
-                        <label>Numero de pedido:</label>
-                        <input
-                        type="number"
-                        className="form-control"
-                        id="nroPedido"
-                        placeholder="Numero de pedido"
-                        min="0"
-                        value = {this.state.cuit}
-                        onChange={this.handleChange}
-                        />
-                        <button type="button" className="btn btn-primary" onClick={buscarPedido}>
-                         Buscar
-                        </button>
-                    </div>
-               </div>
-               <div>                    
-                    {pedido}
-                    {items}
-                </div>
-            </form>
-        )
+          <tbody key={index}>
+            <tr>
+              <td>{i.producto.nombre}</td>
+              <td>${i.producto.precio.toFixed(2)}</td>
+              <td>{i.cantidad}</td>
+            </tr>
+          </tbody>
+        );
+      });
+      items = (
+        <div>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Producto</th>
+                <th>Precio unitario</th>
+                <th>Cantidad</th>
+              </tr>
+            </thead>
+            {i}
+          </table>
+        </div>
+      );
     }
+    return (
+      <div>
+        <form>
+          <div className="form-group row">
+            <div className="col-6">
+              <label>Numero de pedido:</label>
+              <input
+                type="number"
+                className="form-control"
+                id="nroPedido"
+                placeholder="Numero de pedido"
+                min="0"
+                value={this.state.cuit}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={buscarPedido}
+            >
+              Buscar
+            </button>
+          </div>
+        </form>
+        <div>
+          {pedido}
+          {items}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Pedidos
+export default Pedidos;
